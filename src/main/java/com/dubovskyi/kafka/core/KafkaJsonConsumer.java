84da3dd5faeaa30fff565cf2d23dfceb4668b467
Mykhailo <m@m.ua>
2019-01-08 22:31:31 +0200

    add Json consumer

--- src/main/java/com/dubovskyi/kafka/core/KafkaJsonConsumer.java
+++ src/main/java/com/dubovskyi/kafka/core/KafkaJsonConsumer.java
@@ -3,13 +3,12 @@ package com.dubovskyi.kafka.core;
 import com.fasterxml.jackson.databind.JsonNode;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.serialization.StringDeserializer;
-import org.apache.kafka.common.serialization.StringSerializer;
 import org.apache.kafka.connect.json.JsonDeserializer;
 
 import java.util.Arrays;
 import java.util.Properties;
 
-public class KafkaConsumers {
+public class KafkaJsonConsumer {
 
     public static void main(String[] args) {
         Properties prop = new Properties();
@@ -17,7 +16,7 @@ public class KafkaConsumers {
 
         prop.setProperty("bootstrap.servers","localhost:9092");
         prop.setProperty("key.deserializer", StringDeserializer.class.getName());
-        prop.setProperty("value.deserializer", StringDeserializer.class.getName());
+        prop.setProperty("value.deserializer", JsonDeserializer.class.getName());
         prop.setProperty("group.id", "testf2");
         prop.setProperty("enable.auto.commit", "true");
         prop.setProperty("auto.commit.interval.ms", "1000");
@@ -25,7 +24,7 @@ public class KafkaConsumers {
 
 
 
-        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(prop);
+        KafkaConsumer<String, JsonNode> consumer = new KafkaConsumer<String, JsonNode>(prop);
         consumer.subscribe(Arrays.asList("test2"));
 
         consumer.poll(1000).forEach(i -> System.out.println(i.value()));
--- src/main/java/com/dubovskyi/kafka/core/KafkaProducer.java
+++ src/main/java/com/dubovskyi/kafka/core/KafkaProducer.java
@@ -4,13 +4,14 @@ import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 import java.util.Properties;
+import java.util.concurrent.ExecutionException;
 
 public class KafkaProducer
 {
     public static void main(String[] args) {
         Properties prop = new Properties();
 
-        prop.setProperty("bootstrap.servers","192.168.99.100:9092");
+        prop.setProperty("bootstrap.servers","localhost:9092");
         prop.setProperty("key.serializer", StringSerializer.class.getName());
         prop.setProperty("value.serializer", StringSerializer.class.getName());
         prop.setProperty("acks","1");
@@ -20,11 +21,25 @@ public class KafkaProducer
 
         Producer<String,String> producer= new org.apache.kafka.clients.producer.KafkaProducer<String, String>(prop);
 
-        ProducerRecord<String,String> record = new ProducerRecord<String, String>("third_topic","2","this is the test message1");
+        ProducerRecord<String,String> record = new ProducerRecord<String, String>("js1","{ \"a\": 1 }");
 
+
+        long start1 = System.currentTimeMillis();
         for(int i =0;i<= 10;i++){
-            producer.send(record);
+            try {
+                long start = System.currentTimeMillis();
+                long offset =  producer.send(record).get().offset();
+
+                System.out.println("send time: "+ (System.currentTimeMillis() - start) +" for offest : "+offset);
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
         }
+
+        System.out.println("total sending time: "+ (System.currentTimeMillis() - start1));
 
 
      //   producer.flush();
